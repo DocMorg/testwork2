6d90a88380ef8d8556a194830f03b090c30d6645
DocMorg <vlad_ustimov@mail.ru>
2019-10-29 11:18:27 +0300

    added flask mail

--- config.py
+++ config.py
@@ -7,3 +7,13 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'postgresql://localhost/testwork?user=postgres&password=toor'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+MAIL_USERNAME = 'your-gmail-username'
+MAIL_PASSWORD = 'your-gmail-password'
+
+# administrator list
+ADMINS = ['your-gmail-username@gmail.com']
