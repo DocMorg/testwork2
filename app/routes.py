50012449020a5360e0fc1c6274528288417240e7
DocMorg <vlad_ustimov@mail.ru>
2019-10-29 10:58:57 +0300

    fix small problems

--- app/routes.py
+++ app/routes.py
@@ -15,9 +15,9 @@ def submit():
         new.id = uuid.uuid1().hex
         new.url = request.form.get('url')
         if new.url:
-            process = subprocess.Popen([sys.executable, 'app\hashing.py'] 
-                       + [new.id, new.url], 
-                       stdout=subprocess.PIPE, 
+            subprocess.Popen([sys.executable, 'app\hashing.py']
+                       + [new.id, new.url],
+                       stdout=subprocess.PIPE,
                        stderr=subprocess.STDOUT,
                        env=os.environ.copy())
         else:
@@ -32,13 +32,11 @@ def submit():
 @app.route("/check", methods=['GET'])
 def check():
     if request.method == "GET":
-        id = request.form.get('id')
-    data = Data.query.get(id)
+        id1 = request.form.get('id')
+    data = Data.query.get(id1)
     if not data:
-        return('Error 404 id not found') 
+        return('Error 404 id not found')
     if data.state == 'finished':
         return('state: ' + data.state + ', \nhash: ' + data.md5_hash)
     else:
         return('state: ' + data.state)
-
-    
